afdb7e1d5115c40ba265e3d56d733242c1aabf3d
Jonathan Perichon <jperichon@gmail.com>
2015-09-25 14:37:00 -0700

    Add Connect with Check button

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'tilt/erb'
 
+CHECKR_CLIENT_ID     = 'XX'
+CHECKR_CLIENT_SECRET = 'XX'
+CHECKR_REDIRECT_URL  = 'http://localhost:9292/oauth/callback'
+CHECKR_AUTHORIZE_URL = 'https://dashboard.checkr.com/oauth/authorize'
+
 get '/' do
-  erb :index
+  erb :index, layout: :layout
+end
+
+get '/oauth_callback' do
+  erb :oauth_callback, layout: :layout
 end
